105d5f4dc50b65ecdf97adbc75feb76578333fd7
User <user@Users-MacBook-Air.local>
2022-03-21 22:30:20 +0100

    created the personnel class and the person class

---
+++ src/main/java/model/Personnel.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.Date;
+
+
+public class Personnel extends Person{
+
+    private Date dateOfBirth;
+    private String admissionNumber;
+    private double height;
+    private double weight;
+    private String genoType;
+    private String bloodGroup;
+
+
+}
