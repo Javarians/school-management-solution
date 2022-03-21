c989eb6f28e538e8966b187977690aa32caf6156
Buezman <chibueze.nwajiobi@decagon.dev>
2022-03-21 22:29:36 +0100

    added subject and student classes

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -1,4 +1,12 @@
 package model;
 
-public class Student {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Student extends Personnel{
+    String currentClass;
 }
--- src/main/java/model/Subject.java
+++ src/main/java/model/Subject.java
@@ -1,4 +1,17 @@
 package model;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@ToString
 public class Subject {
+    String title;
+    double classwork;
+    double homework;
+    double examination;
 }
