a239ac680052234a2b57cb2163524b81fa37914d
User <user@Users-MacBook-Air.local>
2022-03-21 23:47:48 +0100

    finger crossed

--- src/main/java/model/Subject.java
+++ src/main/java/model/Subject.java
@@ -1,5 +1,12 @@
 package model;
-
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+@Getter
+@Setter
+@RequiredArgsConstructor
+@ToString
 public class Subject {
     String title;
     double classwork;
